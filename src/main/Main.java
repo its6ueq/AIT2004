2d0485366d3119652eed1cebeb7b0d1f0e569fdd
its6ueq <totenhaibang@gmail.com>
2024-09-04 22:50:40 +0700

    Sep4

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 public class Main {
     public static void main (String[] args) {
-        System.out.println ("Hello world!");
+        JFrame window = new JFrame ("Simple Chess");
+        window.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        window.setResizable (false);
+
+        GamePanel gp = new GamePanel ();
+        window.add(gp);
+        window.pack();
+
+        window.setLocationRelativeTo (null);
+        window.setVisible (true);
+
+        gp.launchGame ();
     }
 }
