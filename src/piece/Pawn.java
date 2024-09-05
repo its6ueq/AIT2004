bc76a08637d9bc4f5a8dd455e63c084355b9836d
NaHieu2005 <145358847+NaHieu2005@users.noreply.github.com>
2024-09-05 23:58:37 +0700

    aahhh

--- src/piece/Pawn.java
+++ src/piece/Pawn.java
@@ -1,7 +1,16 @@
 package piece;
 
+import main.GamePanel;
+
 public class Pawn extends Piece{
     public Pawn (int color, int col, int row){
+        super(color, col, row);
 
+        if (color == GamePanel.WHITE){
+            image = getImage("piece/w-pawn.png");
+        }
+        else {
+            image = getImage("piece/b-pawn.png");
+        }
     }
 }
