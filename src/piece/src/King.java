64d7bf7a5b7e1b4bfd732aa92de4cd0433bc1a2c
TudRVN <149549744+TudRVN@users.noreply.github.com>
2024-09-05 09:32:49 +0700

    hh

--- src/piece/src/King.java
+++ src/piece/src/King.java
@@ -1,2 +1,19 @@
-public class King
-{}
+package piece;
+import main.GamePanel;
+public class King extends Piece {
+    public King (int col, int col, int row) {
+        super(color, col, row);
+        if (color == GamePanel.WHITE) {
+            image = getImage("/piece/w-king");
+        }
+        else {
+            image = getImage("/piece/b-king");
+        }
+    }
+    public boolean canMove(int targetCol, int targetRow) {
+        if (isWithinBoard(targetCol, targetRow)) {
+            if (Math.abs(targetCol - ))
+        }
+    }
+}
+
