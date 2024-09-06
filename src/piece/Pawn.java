5529f8abf44551592c4b9cfa99f67cee1ee4df07
NaHieu2005 <145358847+NaHieu2005@users.noreply.github.com>
2024-09-06 10:40:24 +0700

    fix

--- src/piece/Pawn.java
+++ src/piece/Pawn.java
@@ -1,16 +1,15 @@
 package piece;
-<<<<<<< HEAD
 import main.GamePanel;
 public class Pawn extends Piece {
     public Pawn(int color, int col, int row) {
         super(color, col, row);
         if (color == GamePanel.WHITE) {
             image = getImage("/piece/w-pawn");
-        }
-        else {
+        } else {
             image = getImage("/piece/b-pawn");
         }
     }
+
     public boolean canMove(int targetCol, int targetRow) {
         if (isWithinBoard(targetCol, targetRow) && !isSameSquare(targetCol, targetRow)) {
             int moveValue;
@@ -31,12 +30,5 @@ public class Pawn extends Piece {
             }
         }
         return false;
-=======
-
-public class Pawn extends Piece{
-    public Pawn (int color, int col, int row){
-        super (color, col, row);
-
->>>>>>> d0cd743a69d19a56832ed6636752cb6923361a01
     }
 }
